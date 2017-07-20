074dd4992663fa44ba7b252a4c8eb4609f937d66
Patrick Stephan <pstephan1187@gmail.com>
2017-07-20 16:15:27 -0500

    Improved display and added descriptions to options.

--- src/Commands/Compare.php
+++ src/Commands/Compare.php
@@ -8,7 +8,12 @@ use SchemaManager\Manager;
 
 class Compare extends Command
 {
-    protected $signature = 'schema-manager:compare {--connection=} {--directory=} {--R|run} {--D|include-deletes}';
+    protected $signature = 'schema-manager:compare '.
+        '{--connection= : The database connection to use.} '.
+        '{--directory= : The directory to look for schema files.} '.
+        '{--R|run : Whether or not to run the migrations. Displays a preview by default.} '.
+        '{--D|include-deletes : Whether or not to include drop table statements. Excludes by default.} '.
+        '{--P|plain-display : Display the statements as a table or plain text.}';
     protected $description = 'Compares your database against your definition files.';
 
     public function handle()
@@ -17,6 +22,7 @@ class Compare extends Command
         $directory = $this->option('directory') ?: database_path('schemas');
         $run = $this->option('run');
         $include_deletes = $this->option('include-deletes');
+        $plain_display = $this->option('plain-display');
         $statements = [];
 
         $manager = app(Manager::class);
@@ -37,10 +43,7 @@ class Compare extends Command
         $statements = $manager->getSqlFor($connection, $include_deletes);
 
         if(!$run){
-            $this->info('Preview');
-            foreach($statements as $statement){
-                $this->line($statement);
-            }
+            $this->showPreview($statements, !$plain_display);
 
             return;
         }
@@ -58,6 +61,56 @@ class Compare extends Command
             array_filter($filesystem->allFiles($directory), function($file){
                 return preg_match('/Schema\.php$/', $file);
             })
+        );
+    }
+
+    protected function showPreview($statements, $show_as_table = true)
+    {
+        $statements = (array) $statements;
+
+        if(count($statements) === 0){
+            $this->info('No changes needed.');
+
+            return;
+        }
+
+        $alters = 0;
+        $creates = 0;
+        $drops = 0;
+
+        foreach($statements as $statement){
+            if(starts_with($statement, 'ALTER TABLE')){
+                $alters++;
+            }
+
+            if(starts_with($statement, 'CREATE TABLE')){
+                $creates++;
+            }
+
+            if(starts_with($statement, 'DROP TABLE')){
+                $drops++;
+            }
+        }
+
+        if($show_as_table){
+            // Turn the array of statments into an array of arrays for the table display
+            // ['statement_1', 'statement_2'] -> [['statement_1'], ['statement_2']]
+            $statements = array_map(function($statement){
+                return [$statement];
+            }, $statements);
+
+            $this->table(['Statements'], $statements);
+        }else{
+            $this->info('Statements:');
+
+            foreach($statements as $statement){
+                $this->line($statement);
+            }
+        }
+
+        $this->table(
+            ['Alters', 'Creates', 'Drops'],
+            [[$alters, $creates, $drops]]
         );
     }
 }
